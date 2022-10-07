6563cdf4462bf2c4ceb225af80839cb0fbd2edd4
awnish04 <64547504+awnish04@users.noreply.github.com>
2022-10-07 13:34:41 +0545

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,45 @@
+// Select The Elements
+
+var toggle_btn ;
+var big_wrapper ;
+
+
+function declare(){
+    toggle_btn = document.querySelector(".toggle-btn");
+    big_wrapper = document.querySelector(".big-wrapper");
+}
+
+
+const main = document.querySelector("main");
+
+declare();
+
+let dark = false
+function toggleAnimation() {
+    // Clone The Wrapper
+    dark = !dark;
+    let clone=big_wrapper.cloneNode(true);
+    if(dark){
+        clone.classList.remove("Light");
+        clone.classList.add("dark");
+    }else{
+        clone.classList.remove("dark");
+        clone.classList.add("Light");
+    }
+    clone.classList.add("copy");
+    main.appendChild(clone);
+
+    clone.addEventListener("animationend",()=>{
+        big_wrapper.remove();
+        clone.classList.remove("copy");
+        // Reset variables
+        declare();
+        events();
+    });
+}
+
+function events(){
+    toggle_btn.addEventListener("click" ,toggleAnimation);
+}
+
+events();
